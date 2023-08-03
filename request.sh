ccdd9dfcc6180e3ecd68e10e69fb14360f5f85d5
darcydai <darcydarcy@foxmail.com>
2023-08-03 14:28:54 +0800

    remove request.sh

--- request.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl --max-time 5 -XGET http://127.0.0.1:8080/first
--- upstream-service/src/main/java/com/example/upstreamservice/controllers/SecondController.java
+++ upstream-service/src/main/java/com/example/upstreamservice/controllers/SecondController.java
@@ -1,11 +1,11 @@
 package com.example.upstreamservice.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
 
-@Controller
+@RestController
 public class SecondController {
     @GetMapping("/second")
     public String second() throws InterruptedException {
